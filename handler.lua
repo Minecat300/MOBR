f1367d879e48bd52d65c88e9cd159ed7a2b07cd6
Minecat300 <120320914+Minecat300@users.noreply.github.com>
2026-03-24 17:43:24 +0100

    Add mobrUtils and enforce URL scheme
    Add mobrUtils with a startsWith helper and update handler.lua to prepend http:// when a URL lacks an http or https scheme. Bump package version to 1.0.3 in install.json.

--- handler.lua
+++ handler.lua
@@ -1,10 +1,13 @@
--- For Open Computers
-
 local internet = require("internet")
+local mobrUtils = require("mobrUtils")
 
 local handler = {}
 
 function handler.go(url)
+    if (not mobrUtils.startsWith(url, "http://") and not mobrUtils.startsWith(url, "https://")) then
+        url = "http://" .. url
+    end
+
     local handle = internet.request(url)
 
     for chunk in handle do
---
+++ mobrUtils.lua
@@ -0,0 +1,5 @@
+local mobrUtils = {}
+
+function mobrUtils.startsWith(str, start)
+    return str:sub(1, #start) == start
+end
