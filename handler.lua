835477def94f6cbe1a63ea5221dc4adeb7302e4f
Minecat300 <120320914+Minecat300@users.noreply.github.com>
2026-03-24 17:52:18 +0100

    Export mobrUtils, simplify handler, bump version
    Add a proper return in mobrUtils.lua so the utility table is exported (fixes require usage). Simplify handler.go by removing the intermediate fixedUrl variable and normalizing the url in-place before calling internet.request. Bump package version to 1.0.5 in install.json. Also add missing EOF newline in mobrUtils.lua.

--- handler.lua
+++ handler.lua
@@ -4,12 +4,11 @@ local mobrUtils = require("mobrUtils")
 local handler = {}
 
 function handler.go(url)
-    local fixedUrl = url
     if (not mobrUtils.startsWith(url, "http://") and not mobrUtils.startsWith(url, "https://")) then
-        fixedUrl = "http://" .. url
+        url = "http://" .. url
     end
 
-    local handle = internet.request(fixedUrl)
+    local handle = internet.request(url)
 
     for chunk in handle do
         print(chunk)
--- mobrUtils.lua
+++ mobrUtils.lua
@@ -3,3 +3,5 @@ local mobrUtils = {}
 function mobrUtils.startsWith(str, start)
     return str:sub(1, #start) == start
 end
+
+return mobrUtils
