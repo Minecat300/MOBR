334245e33297f69b764de549798f7b9f3d2cd63f
Minecat300 <120320914+Minecat300@users.noreply.github.com>
2026-03-24 18:07:05 +0100

    Print HTTP response headers; bump version
    Add printHeaders(url) to call internet.request with a callback that prints response status and headers (or a "No headers received" message). Replace the previous streaming behavior in handler.go to use printHeaders (previous chunk-streaming code left commented for reference). Also bump package version in install.json from 1.0.5 to 1.0.6.

--- handler.lua
+++ handler.lua
@@ -3,16 +3,35 @@ local mobrUtils = require("mobrUtils")
 
 local handler = {}
 
+local function printHeaders(url)
+    internet.request(url, nil, function(_, response)
+        if response then
+            print("Status:", response.status)
+
+            if response.headers then
+                for k, v in pairs(response.headers) do
+                    print(k .. ":", v)
+                end
+            else
+                print("No headers received")
+            end
+        end
+    end)
+end
+
 function handler.go(url)
     if (not mobrUtils.startsWith(url, "http://") and not mobrUtils.startsWith(url, "https://")) then
         url = "http://" .. url
     end
 
-    local handle = internet.request(url)
+    return printHeaders(url)
 
-    for chunk in handle do
-        print(chunk)
-    end
+    -- local handle = internet.request(url)
+
+    -- for chunk in handle do
+    --     print(chunk)
+    -- end
+
 end
 
 return handler
